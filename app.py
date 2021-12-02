1cef5c2bbdb9f0b33fba62386e8e38661c3bd730
Song-EunJu <jd06280@naver.com>
2021-12-02 21:25:48 +0900

    [feat] 완성영상 업로드 후 비디오번호 리턴

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask_restful import reqparse
 from flask import Flask, jsonify, request, render_template
-# from sqlalchemy import create_engine, text
 from mod_dbconn import mod_dbconn
 
 import pymysql
-import numpy as np
-import pickle as p
 import json
 import os
+import pathlib
+import shutil
+
 
 app = Flask(__name__)
 
@@ -49,29 +49,39 @@ def getVideoId():
                         WHERE _id=%s"
         row = db_class.executeOne(sql,videoId)
         db_class.commit()
-        print(row)
         print(row['video_path']) # 로컬에서 비디오가 어디에 저장되어 있는지
-
         # fileUpload()  함수로 빼고 싶긴 함
 
-        directory = "C:/Users/Windows10/GaoriProcessedVideos"
-        if not os.path.exists(): # 폴더가 없는 경우 생성하기
-            os.makedirs(directory)
+        # pathlib.Path.home: 사용자의 홈 디렉토리(~) ex) C:\Users\Windows10
+        path = str(pathlib.Path.home())+"\GaoriProcessedVideos" # 새로 저장할 폴더
+        if not os.path.exists(path): # 폴더가 없는 경우 생성하기
+            os.makedirs(path)
         else: # 폴더가 있는 경우 폴더에 동영상 업로드
-            processed_video_name = '바꾼 이름'
-            saved_video_name = row['saved_video_name']
-            video_path = row['video_path']
+            processed_video_name = row['origin_video_name'] # '방구석 수련회 장기자랑 - YouTube - Chrome 2021-11-05 04-18-36.mp4'
+            # 구현 완료 후 : 동영상 이름을 지정
 
-            # 기존 영상을 가져와서 폴더에 저장한 다음에 db에 그 경로 저장해줘야 함
+            saved_video_name = row['saved_video_name']
+            # 'af4d1f8d24169c2a8988625d4f2e3455.mp4' ( 완성영상도 같은 이름으로 저장한다고 가정)
+
+            video_path = row['video_path'] # 'C:\\Users\\Windows10\\GaoriVideos\\af4d1f8d24169c2a8988625d4f2e3455.mp4'
+            # 구현 완료 후에는 이게 필요없을 듯
+
+            shutil.copy(video_path, path) # 일단은 기존 영상을 새로운 폴더에 복사하는 형태로 해둠
+
+            new_video_path = "{}\{}".format(path, saved_video_name) # 완성 영상이 저장될 새로운 경로
 
             sql = "INSERT INTO processed_video(processed_video_name, saved_video_name, video_path) \
-                    VALUES(%s, %s, %s)"
-            val = (processed_video_name, saved_video_name, video_path)
+                    VALUES(%s, %s, %s)" # 완료된 영상을
+            val = (processed_video_name, saved_video_name, new_video_path)
             row = db_class.executeOne(sql, val)
             db_class.commit()
-            print(row)
+            find_sql = "SELECT * \
+                                    FROM processed_video \
+                                    WHERE saved_video_name=%s"
+            row = db_class.executeOne(find_sql, saved_video_name) # 일단은 저장한 애를 saved_video_name으로 찾아옴
+            db_class.commit()
 
-        return jsonify({'result': 'send completely'})
+        return jsonify({'videoId':row['_id']}) # 방금 저장한 videoId를 리턴
     else:
         return jsonify({'result': 'send completely'})
 
