3eb5e9f80463b032492bed941f50e36e1fa62a9a
Song-EunJu <jd06280@naver.com>
2021-12-26 22:38:23 +0900

    [fix] blurring 원형 블러 수정

--- faceBlurring.py
+++ faceBlurring.py
@@ -16,7 +16,7 @@ def blurr_apply(lst_all, lst_remove,cap,pathandName):
 
     for i in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))):
         retval, frame = cap.read()
-        # 일단 같은 프레임인지 찾는다. 근데 lst_all 엄청 길텐데 그럼 효율이 쓰레기가 되지 않을까.ㅎ
+        # 일단 같은 프레임인지 찾는다. 근데 lst_all 엄청 길텐데 그럼 효율이 쓰레기가 되지 않을까
         for t in range(0, len(lst_all)):
             # 프레임의 번호가 같을때
             if lst_all[t][4] == i:
@@ -29,27 +29,27 @@ def blurr_apply(lst_all, lst_remove,cap,pathandName):
 
                 # 블러처리하는 부분 입니다,
                 if check == True:
-                    height = abs(lst_all[t][1] - lst_all[t][3])
-                    width = abs(lst_all[t][0] - lst_all[t][2])
-                    yPos = min(lst_all[t][1], lst_all[t][3])
-                    xPos = min(lst_all[t][0], lst_all[t][2])
+                    height = abs(int(lst_all[t][1]) - int(lst_all[t][3]))
+                    width = abs(int(lst_all[t][0]) - int(lst_all[t][2]))
+                    yPos = min(int(lst_all[t][1]), int(lst_all[t][3]))
+                    xPos = min(int(lst_all[t][0]), int(lst_all[t][2]))
 
                     face_img = frame[yPos:yPos + height, xPos:xPos + width]  # 탐지된 얼굴 크롭
                     c_mask = np.zeros((height, width), np.uint8)
-                    cv2.circle(c_mask, (width, height), max(width, height), 1, thickness=-1)
+                    cv2.circle(c_mask, (width // 2, height // 2), max(width, height) // 2, 1, thickness=-1)
+
                     mask = cv2.bitwise_and(face_img, face_img, mask=c_mask)
                     img_mask = face_img - mask
                     blur = cv2.blur(face_img, (11, 11))
                     mask2 = cv2.bitwise_and(blur, blur, mask=c_mask)  # mask
 
                     final_img = img_mask + mask2
-
                     frame[yPos:yPos + height, xPos:xPos + width] = final_img
+
+
             # 이 부분이 있는 이유는 조금이라도 효율을 높이고자 프레임 값이 현재 프레임보다 1 크면 걍 뒤에있는 모든 배열 값을 넘겨버린다! 이겁니다!
             elif lst_all[t][5] == i + 1:
                 t = len(lst_all)
-
-        # print(i)
         out.write(frame)
     # files.download('output.avi')
     # pp.imshow(frame)
